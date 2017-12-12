84dfca68456be1991db769782847789edb85fb60
the.lumw@gmail.com <the.lumw@gmail.com>
2017-12-13 02:37:57 +0800

    2017-12-13 v2

--- src/user_1.0.0.js
+++ src/user_1.0.0.js
@@ -5,44 +5,40 @@
 /**
  * @apidefine request_User
  * @apiparam {Object} user
- * @apiParam {String} user.userName
- * @apiParam {Number} user.nickName
- * @apiParam {String} user.weChatNickName
- * @apiParam {String} user.wechatId
- * @apiParam {String} user.gender
- * @apiParam {Number} [user.age]
+ * @apiParam {String} [user.userName] 用户名
+ * @apiParam {Number} [user.nickName] 昵称
+ * @apiParam {String} user.wechatId 微信ID
+ * @apiParam {String} [user.gender] 性别 0-女 1-男
+ * @apiParam {Number} [user.birthday] yyyy/MM/dd
  * @apiParam {Number} [user.email]
  * @apiParam {Number} [user.phoneNumber]
  * @apiParam {Number} [user.address]
  * @apiParam {String} [user.province]
  * @apiParam {String} [user.city]
+ * @apiParam {Number} user.recCreateAppro 用户创建方式  默认 0-微信
  */
 
 
 /**
  * @apidefine response_Create_User
  * @apiSuccess {Object} user
- * @apiSuccess {Number} user.userId
- * @apiSuccess {String} user.userName
- * @apiSuccess {Number} user.nickName
- * @apiSuccess {String} user.weChatNickName
- * @apiSuccess {String} user.wechatId
- * @apiSuccess {String} user.gender
- * @apiSuccess {Number} [user.age]
+ * @apiSuccess {Number} user.userId 用户唯一标识
+ * @apiSuccess {String} [user.userName] 用户名
+ * @apiSuccess {Number} [user.nickName] 昵称
+ * @apiSuccess {String} user.wechatId 微信ID
+ * @apiSuccess {String} [user.gender] 性别 0-女 1-男
+ * @apiSuccess {Number} [user.birthday] yyyy/MM/dd
  * @apiSuccess {Number} [user.email]
  * @apiSuccess {Number} [user.phoneNumber]
  * @apiSuccess {Number} [user.address]
  * @apiSuccess {String} [user.province]
  * @apiSuccess {String} [user.city]
- * @apiSuccess {String} [user.rec_cteat_dt_tm]
- * @apiSuccess {String} [user.rec_upt_dt_tm]
+ * @apiSuccess {Number} user.recCreateAppro 用户创建方式  默认 0-微信
  * @apiSuccess {Object} account[]
  * @apiSuccess {Number} account.accountId
- * @apiSuccess {String} account.accountType
+ * @apiSuccess {String} account.accountType 1-游戏币账户 2-积分账户
  * @apiSuccess {String} account.userId
  * @apiSuccess {Number} account.balance
- * @apiSuccess {String} account.recCreatDtTm
- * @apiSuccess {String} account.recUptDtTm
  * @apiSuccess {Number} account.status
  *
  * @apiError {String} reasonCode
@@ -52,23 +48,35 @@
 /**
  * @apidefine response_Retrieve_User
  * @apiSuccess {Object} user
- * @apiSuccess {Number} user.userId
- * @apiSuccess {String} user.userName
- * @apiSuccess {Number} user.nickName
- * @apiSuccess {String} user.weChatNickName
- * @apiSuccess {String} user.wechatId
- * @apiSuccess {String} user.gender
- * @apiSuccess {Number} [user.age]
+ * @apiSuccess {Number} user.userId 用户唯一标识
+ * @apiSuccess {String} [user.userName] 用户名
+ * @apiSuccess {Number} [user.nickName] 昵称
+ * @apiSuccess {String} user.wechatId 微信ID
+ * @apiSuccess {String} [user.gender] 性别 0-女 1-男
+ * @apiSuccess {Number} [user.birthday] yyyy/MM/dd
  * @apiSuccess {Number} [user.email]
  * @apiSuccess {Number} [user.phoneNumber]
  * @apiSuccess {Number} [user.address]
  * @apiSuccess {String} [user.province]
  * @apiSuccess {String} [user.city]
- * @apiSuccess {String} [user.rec_cteat_dt_tm]
- * @apiSuccess {String} [user.rec_upt_dt_tm]
+ * @apiSuccess {Number} user.recCreateAppro 用户创建方式 默认 0-微信
+ * @apiSuccess {Object} account[]
+ * @apiSuccess {Number} account.accountId
+ * @apiSuccess {String} account.accountType 1-游戏币账户 2-积分账户
+ * @apiSuccess {String} account.userId
+ * @apiSuccess {Number} account.balance
+ * @apiSuccess {Number} account.status
  *
  * @apiError {String} reasonCode
  */
+
+/**
+ * @apidefine request_User_Login
+ * @apiparam {Object} userLogin
+ * @apiParam {String} userLogin.wechatId 微信ID
+ * @apiParam {Number} userLogin.recCreateAppro 用户创建方式  默认 0-微信
+ */
+
 
  /**
   * @api {post} /poseidon/user/ Create User
@@ -108,3 +116,15 @@
   * @apiuse response_Retrieve_User
   */
 
+
+/**
+ * @api {post} /poseidon/user/login User Login
+ * @apiName UserLogin
+ * @apiVersion 1.0.0
+ * @apiGroup Group_User
+ *
+ * @apiuse HeaderParameters
+ * @apiuse request_User_Login
+ *
+ * @apiuse response_Create_User
+ */
